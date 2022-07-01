359f2383b1ac7db41e95d8280fface95856f2f94
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-07-02 05:58:45 +0800

    initial commit

---
+++ components/ProfileHeroBanner/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProfileHeroBanner = () => {
+  return (
+    <div>
+      <div>
+        <img width={"100%"} src="/images/profile-hero-banner.png" />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHeroBanner;
---
+++ pages/profile/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import ProfileDetails from "../../components/ProfileDetails";
+import ProfileHeroBanner from "../../components/ProfileHeroBanner";
+// import ProfilePicture from "../../components/ProfilePicture";
+// import ProfileShoutOut from "../../components/ProfileShoutOut";
+// import Testimonials from "../../components/Testimonials";
+// import UserConnections from "../../components/UserConnections";
+import classes from "./styles.module.scss";
+
+const ProfilePage = () => {
+  return (
+    <div className="pageLayout">
+      <div className={classes.profileHeaderContainer}>
+        <ProfileHeroBanner />
+        {/* <ProfilePicture />
+        <ProfileShoutOut />
+        <ProfileDetails /> */}
+      </div>
+      {/* <div className={classes.mainContentContainer}>
+        <div className={classes.testimonialsContainer}>
+          <Testimonials />
+        </div>
+        <div className={classes.userConnectionsContainer}>
+          <UserConnections />
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default ProfilePage;
